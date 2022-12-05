42a58a2e92a7a37829741e231d5e9ab656a28bd1
unknown <21b01a1243@svecw.edu.in>
2022-12-05 08:54:56 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,6 @@
+const arr=["krishna","hima","lakshmi",]
+console.log(arr.push("family"))
+console.log(arr.unshift("Gayathri"))
+console.log(arr.pop())
+console.log(arr.shift())
+console.log(arr.sort())
